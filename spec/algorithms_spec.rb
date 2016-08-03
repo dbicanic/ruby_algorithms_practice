cdb000942622b494f63c5ec829023c3d9b6de662
dbicanic <dbicanic77@gmail.com>
2016-08-03 12:59:54 -0500

    Made tests and algorithm to past factoral problem

--- spec/algorithms_spec.rb
+++ spec/algorithms_spec.rb
@@ -2,7 +2,11 @@ require_relative('../algorithms')
 
 describe 'finding the factorals' do
 	it 'returns 24 when 4 is entered' do 
-		expect(FirstFactorial(4)).to eq 4
+		expect(FirstFactorial(4)).to eq 24
+	end
+
+	it 'returns 40320 when 8 is entered' do 
+		expect(FirstFactorial(8)).to eq 40320
 	end
 end
 
