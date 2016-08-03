790f5ca699353bb980410664c435b258a9d275b2
dbicanic <dbicanic77@gmail.com>
2016-08-03 12:47:31 -0500

    Add spec and algorithms file, made first test, haven't tested to see if it works

---
+++ spec/algorithms_spec.rb
@@ -0,0 +1,10 @@
+require_relative('../algorithms')
+
+describe 'finding the factorals' do
+	it 'returns 24 when 4 is entered' do 
+		expect(FirstFactorial(4)).to eq 4
+	end
+end
+
+describe 'Converting a sentence to Pig Latin' do
+end
